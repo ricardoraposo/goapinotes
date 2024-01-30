c4c84a2346726186e097215d0eda9fe3c2fd38d8
ricardo <ricardorbxx1@gmail.com>
2024-01-30 19:44:02 -0300

    something else

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -36,6 +36,7 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
+    r.Use(middleware.Recoverer)
 
 	r.Route("/products", func(r chi.Router) {
         r.Use(jwtauth.Verifier(c.TokenAuth))
